b2650ddef717d83eee4ce85bad8e58df4bb919a2
martin.anzinger <martin.anzinger@0a77ce28-79b5-11de-a1b3-85d0aeae5c5b>
2009-09-23 14:06:31 +0000

    updated headers

--- src/Webinterface/comTask.h
+++ src/Webinterface/comTask.h
@@ -1,7 +1,16 @@
 /*
-defines the Communication Task (vComTask) to interact with the machine
+ * comTask.h $Rev$ $Author$ $LastChangedDate$
+ *
+ * (c) 2009 Anziner, Hahn
+ * GPV v3
+ *
+ * defines the Communication Task (vComTask) to interact with the machine
 */
 
+#include "lcd_message"  /* Include xOLEDMessage */
+
+
 #define Com_TASK_STACK_SIZE			( configMINIMAL_STACK_SIZE + 10 )
+
 
 void vComTask( void *pvParameters );
