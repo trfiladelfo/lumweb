75c2242a46d8a5a1372ffd231c8a86f7b736eb70
d3f3nd3r@bytegeist.org <d3f3nd3r@bytegeist.org@0a77ce28-79b5-11de-a1b3-85d0aeae5c5b>
2009-09-23 14:15:12 +0000

    set svn:keywords on 'Date Author Revision'

--- src/Webinterface/comTask.h
+++ src/Webinterface/comTask.h
@@ -7,7 +7,7 @@
  * defines the Communication Task (vComTask) to interact with the machine
 */
 
-#include "lcd_message"  /* Include xOLEDMessage */
+#include "lcd_message.h"  /* Include xOLEDMessage */
 
 
 #define Com_TASK_STACK_SIZE			( configMINIMAL_STACK_SIZE + 10 )
