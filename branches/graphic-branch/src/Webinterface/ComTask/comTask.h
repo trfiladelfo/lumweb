ea62f480ceaad17a5f83e26f481db36dc38b860d
d3f3nd3r@bytegeist.org <d3f3nd3r@bytegeist.org@0a77ce28-79b5-11de-a1b3-85d0aeae5c5b>
2009-09-25 08:37:15 +0000

    graphic-branch und dhcpc-branch hinzugefuegt

---
+++ branches/graphic-branch/src/Webinterface/ComTask/comTask.h
@@ -0,0 +1,17 @@
+/*
+ * comTask.h $Rev$ $Author$
+ * $LastChangedDate$
+ *
+ * (c) 2009 Anziner, Hahn
+ * GPV v3
+ *
+ * defines the Communication Task (vComTask) to interact with the machine
+*/
+
+#include "lcd_message.h"  /* Include xOLEDMessage */
+
+
+#define Com_TASK_STACK_SIZE			( configMINIMAL_STACK_SIZE + 10 )
+
+
+void vComTask( void *pvParameters );
