1d2aca7be83e6c220f374cfa84b4873b2c0e0ab8
d3f3nd3r@bytegeist.org <d3f3nd3r@bytegeist.org@0a77ce28-79b5-11de-a1b3-85d0aeae5c5b>
2009-11-04 15:07:34 +0000

    configReader test beispiel

--- trunk/src/Webinterface/Configuration/configReader.c
+++ trunk/src/Webinterface/Configuration/configReader.c
@@ -4,28 +4,24 @@
  *  Created on: 2.1.2009
  *      Author: root
  */
-
+#include <stdio.h>
 
 #include "fs.h"
 
 
-#include "configreader.h"
+#include "configReader.h"
 
 
 int readConfigFile(char *fileName){
 	FILE_HANDLE mainfile;
-	char *msg;
-
-	msg = pvPortMalloc(30);
 
 	FSInit();
 	mainfile = FSOpen("main.txt\0");
 	if (mainfile != NULL) {
-		FSRead(mainfile, msg, 30);
-
-		xGraph_msg.msg = msg;
+	//	FSRead(mainfile, msg, 30);
+;
 
 	} else {
-		xGraph_msg.msg = "READ ERROR!";
+		;
 	}
 }
