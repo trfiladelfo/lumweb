c0ce0abd483b2bcc6d9030dd6766f1607a091a5e
martin.anzinger <martin.anzinger@0a77ce28-79b5-11de-a1b3-85d0aeae5c5b>
2010-02-09 23:23:30 +0000

    neues HTTP-FS

--- src/uInterface/ethernet/httpd/httpd-fs/js/funcs_c.js
+++ src/uInterface/ethernet/httpd/httpd-fs/js/funcs_c.js
@@ -1,27 +1,49 @@
 jx={getHTTPObject:function(){var A=false;if(typeof ActiveXObject!="undefined"){try{A=new ActiveXObject("Msxml2.XMLHTTP")}catch(C){try{A=new ActiveXObject("Microsoft.XMLHTTP")}catch(B){A=false}}}else{if(window.XMLHttpRequest){try{A=new XMLHttpRequest()}catch(C){A=false}}}return A},load:function(url,callback,format){var http=this.init();if(!http||!url){return }if(http.overrideMimeType){http.overrideMimeType("text/xml")}if(!format){var format="text"}format=format.toLowerCase();var now="uid="+new Date().getTime();url+=(url.indexOf("?")+1)?"&":"?";url+=now;http.open("GET",url,true);http.onreadystatechange=function(){if(http.readyState==4){if(http.status==200){var result="";if(http.responseText){result=http.responseText}if(format.charAt(0)=="j"){result=result.replace(/[\n\r]/g,"");result=eval("("+result+")")}if(callback){callback(result)}}else{if(error){error(http.status)}}}};http.send(null)},init:function(){return this.getHTTPObject()}}
 
-function inc(id, max, min){
+function valueOut (elementValue, value, decimal) {
+	/*if (decimal != 0) {
+		elementValue.value = parseFloat(value) / 10;
+	} else { 
+		elementValue.value = parseInt(value);
+	}*/
+
+	elementValue.value = value;
+}
+
+function inc(id, maxVal, minValm, increment, decimal){
 	var element = document.getElementById(id);
-	var n_value = parseInt(element.value) + 1;
-	element.value = n_value;
-	if(max != -1 && min != -1){
-		if(n_value > max)
-			element.value = min;
+	var n_value = parseInt(element.value) + increment;
+	
+	valueOut (element, n_value, decimal); 
+	if(maxVal != -1 && minVal != -1){
+		if(n_value > maxVal)
+			valueOut (element, minVal, decimal);
 	}
 }
 
-function dec(id, max, min){
+function dec(id, maxVal, minVal, increment, decimal){
 	var element = document.getElementById(id);
-	var n_value = parseInt(element.value) - 1;
-	element.value = n_value;
-	if(max != -1 && min != -1){
-		if(n_value < min)
-			element.value = max;
+	var n_value = parseInt(element.value) - increment;
+	
+	valueOut (element, n_value, decimal); 
+	if(maxVal != -1 && minVal != -1){
+		if(n_value < minVal)
+			valueOut (element, maxVal, decimal);
 	}		
 }
 
-function addB(id, max, min){
-	document.write("<input type=\"button\" value=\"+\" onclick=\"inc('"+id+"',"+max+","+min+");\" /><input type=\"button\" value=\"-\" onclick=\"dec('"+id+"',"+max+","+min+");\" />");
+function addB(id, maxParam, minParam, incrementParam, decimal){
+	
+	var maxVal = parseInt (maxParam);
+	var minVal = parseInt (minParam);
+	var increment =  parseInt (incrementParam);
+	/*if (decimal == 0) {
+		maxVal /= 10;
+		minVal /= 10;
+		increment /= 10;
+	}*/
+	document.write("<input type=\"button\" value=\"+\" onclick=\"inc('"+id+"',"+maxVal+","+minVal+", "+increment+", "+decimal+");\" />"+
+		       "<input type=\"button\" value=\"-\" onclick=\"dec('"+id+"',"+maxVal+","+minVal+", "+increment+", "+decimal+");\" />");
 
 }
 
@@ -54,6 +76,7 @@ function getElementsByClass( searchClass, domNode, tagName) {
 function get_form_values(){
 	var get_string = "?";
 	var fields = getElementsByClass('fi');
+
 	for(i=0; i<fields.length; i++){
 		get_string += fields[i].name + "=" + fields[i].value +"&";		
 	}
