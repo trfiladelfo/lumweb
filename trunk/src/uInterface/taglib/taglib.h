995ba58044e3027627f9d69c7bfdaedbd8984f4d
martin.anzinger <martin.anzinger@0a77ce28-79b5-11de-a1b3-85d0aeae5c5b>
2010-03-19 08:07:16 +0000

    taglib changes

--- trunk/src/uInterface/taglib/taglib.h
+++ trunk/src/uInterface/taglib/taglib.h
@@ -21,25 +21,27 @@ static const char * const g_pcConfigTags[] = { "DateTime", /// SSI_INDEX_DATEAND
 };
 
 #define TAG_INDEX_DATEANDTIME  			0
-#define TAG_INDEX_INTEGERINPUTFIELD    	1
-#define TAG_INDEX_SUBMITINPUTFIELD     	2
+#define TAG_INDEX_INTEGERINPUTFIELD    	        1
+#define TAG_INDEX_SUBMITINPUTFIELD     	        2
 #define TAG_INDEX_SAVEDPARAMS    		3
-#define TAG_INDEX_CHECKBOXINPUTFIELD    4
-#define TAG_INDEX_HYPERLINK			    5
-#define TAG_INDEX_TITEL				    6
-#define TAG_INDEX_GROUP				    7
-#define TAG_INDEX_TIMEINPUTFIELD	    8
-#define TAG_INDEX_FLOATINPUTFIELD	    9
+#define TAG_INDEX_CHECKBOXINPUTFIELD            4
+#define TAG_INDEX_HYPERLINK			5
+#define TAG_INDEX_TITEL				6
+#define TAG_INDEX_GROUP				7
+#define TAG_INDEX_TIMEINPUTFIELD	        8
+#define TAG_INDEX_FLOATINPUTFIELD	        9
 
 #define NUM_CONFIG_TAGS     (sizeof(g_pcConfigTags) / sizeof (char *))
 
 typedef struct __taglibStruct {
-	int tagindex;
+	char* tagname;
+
+	void (* renderSSI )(char * pcBuf, int iBufLen, pSSIParam *params);
+
 	void (* onLoad)(char*, int, void* basicDisplayLine);
 	tWidget (* onDisplay) (void* basicDisplayLine, int);
 	void (* onEditValue) (void* basicDisplayLine);
 	void (* onDestroy) (void* basicDisplayLine);
-
 	char (* strFormatter) (void* basicDisplayLine);
 
 	void* userSpace;
