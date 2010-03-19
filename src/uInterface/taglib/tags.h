8abe0c46d5ddcd895caa56d9f6deafb025585f18
martin.anzinger <martin.anzinger@0a77ce28-79b5-11de-a1b3-85d0aeae5c5b>
2010-03-19 08:07:16 +0000

    taglib changes

--- src/uInterface/taglib/tags.h
+++ src/uInterface/taglib/tags.h
@@ -11,25 +11,45 @@
 #include "graphic/gui/displayBasics.h"
 
 // DUMMY FUNCTIONS
-void vDummyOnLoadPtr (char*, int, void* this);
-tWidget* vDummyOnDisplayPtr ( void* this, int);
-void vDummyOnEditValuePtr ( void* this);
-void vDummyOnDestroyPtr ( void* this);
-char* vDummyStrFormatter (void* this);
+void
+vDummyOnLoadPtr(char*, int, void* this);
+
+tWidget*
+vDummyOnDisplayPtr(void* this, int);
+
+void
+vDummyOnEditValuePtr(void* this);
+
+void
+vDummyOnDestroyPtr(void* this);
+
+char*
+vDummyStrFormatter(void* this);
 
 // PRODUCTIVE FUNCTIONS
 
-static const taglib xTagList[] = {
-		{0,	vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}, // DATETIME
-		{1, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}, // INTEGERINPUT
-		{2, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}, // SUBMIT
-		{3, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}, // SAVED PARAMS
-		{4, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}, // CHECKBOX
-		{5, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}, // HYPERLINK
-		{6, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}, // TITLE
-		{7, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}, // GROUP
-		{8, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}, // TIMEINPUT
-		{9, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr, vDummyOnDestroyPtr, vDummyStrFormatter, NULL}  // FLOATINPUT
-};
+static const taglib xTagList[] =
+  {
+    { 0, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+        vDummyOnDestroyPtr, vDummyStrFormatter, NULL }, // DATETIME
+        { 1, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+            vDummyOnDestroyPtr, vDummyStrFormatter, NULL }, // INTEGERINPUT
+        { 2, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+            vDummyOnDestroyPtr, vDummyStrFormatter, NULL }, // SUBMIT
+        { 3, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+            vDummyOnDestroyPtr, vDummyStrFormatter, NULL }, // SAVED PARAMS
+        { 4, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+            vDummyOnDestroyPtr, vDummyStrFormatter, NULL }, // CHECKBOX
+        { 5, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+            vDummyOnDestroyPtr, vDummyStrFormatter, NULL }, // HYPERLINK
+        { 6, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+            vDummyOnDestroyPtr, vDummyStrFormatter, NULL }, // TITLE
+        { 7, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+            vDummyOnDestroyPtr, vDummyStrFormatter, NULL }, // GROUP
+        { 8, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+            vDummyOnDestroyPtr, vDummyStrFormatter, NULL }, // TIMEINPUT
+        { 9, vDummyOnLoadPtr, vDummyOnDisplayPtr, vDummyOnEditValuePtr,
+            vDummyOnDestroyPtr, vDummyStrFormatter, NULL } // FLOATINPUT
+  };
 
 #endif /* TAGS_H_ */
